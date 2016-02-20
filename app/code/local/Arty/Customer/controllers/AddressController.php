852330f6eeb07e3d5b0fb4366f03bbf1f9d42a7c
snehamalvankar <sneha.malvankar@ob10it.com>
2016-02-20 07:51:07 +0530

    nt proprly shutdown wala code

---
+++ app/code/local/Arty/Customer/controllers/AddressController.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sneha
+ * Date: 12/2/16
+ * Time: 9:49 AM
+ */
+require_once(Mage::getModuleDir('controllers','Mage_Customer').DS.'AddressController.php');
+class Arty_Customer_AddressController extends Mage_Customer_AddressController
+{
+    public function indexAction()
+    {
+        if (count($this->_getSession()->getCustomer())) {
+            $this->loadLayout();
+            $this->_initLayoutMessages('customer/session');
+            $this->_initLayoutMessages('catalog/session');
+
+            $block = $this->getLayout()->getBlock('address_book');
+            if ($block) {
+                $block->setRefererUrl($this->_getRefererUrl());
+            }
+            $this->renderLayout();
+        }
+    }
+
+    public function formPostAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*/');
+        }
+        // Save data
+        if ($this->getRequest()->isPost()) {
+            echo '<script>alert("Hiiiii"); </script>';
+            $customer = $this->_getSession()->getCustomer();
+            /* @var $address Mage_Customer_Model_Address */
+            $address  = Mage::getModel('customer/address');
+            $addressId = $this->getRequest()->getParam('id');
+            if ($addressId) {
+                $existsAddress = $customer->getAddressById($addressId);
+                if ($existsAddress->getId() && $existsAddress->getCustomerId() == $customer->getId()) {
+                    $address->setId($existsAddress->getId());
+                }
+            }
+
+            $errors = array();
+
+            /* @var $addressForm Mage_Customer_Model_Form */
+            $addressForm = Mage::getModel('customer/form');
+            $addressForm->setFormCode('customer_address_edit')
+                ->setEntity($address);
+            $addressData    = $addressForm->extractData($this->getRequest());
+            $addressErrors  = $addressForm->validateData($addressData);
+            if ($addressErrors !== true) {
+                $errors = $addressErrors;
+            }
+
+            try {
+                $addressForm->compactData($addressData);
+                $address->setCustomerId($customer->getId())
+                    ->setIsDefaultBilling($this->getRequest()->getParam('default_billing', false))
+                    ->setIsDefaultShipping($this->getRequest()->getParam('default_shipping', false));
+
+                $addressErrors = $address->validate();
+                if ($addressErrors !== true) {
+                    $errors = array_merge($errors, $addressErrors);
+                }
+
+                if (count($errors) === 0) {
+                    $address->save();
+                    $this->_getSession()->addSuccess($this->__('The address has been saved.'));
+                    $this->_redirectSuccess(Mage::getUrl('*/*/index', array('_secure'=>true)));
+                    return;
+                } else {
+                    $this->_getSession()->setAddressFormData($this->getRequest()->getPost());
+                    foreach ($errors as $errorMessage) {
+                        $this->_getSession()->addError($errorMessage);
+                    }
+                }
+            } catch (Mage_Core_Exception $e) {
+                $this->_getSession()->setAddressFormData($this->getRequest()->getPost())
+                    ->addException($e, $e->getMessage());
+            } catch (Exception $e) {
+                $this->_getSession()->setAddressFormData($this->getRequest()->getPost())
+                    ->addException($e, $this->__('Cannot save address.'));
+            }
+        }
+
+        return $this->_redirectError(Mage::getUrl('*/*/index', array('id' => $address->getId())));
+    }
+
+}
---
+++ app/code/local/Arty/Notification/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sneha
+ * Date: 11/2/16
+ * Time: 2:55 PM
+ */ 
+class Arty_Notification_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
